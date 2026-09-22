5ffa7a6e3dc604afcd0136c1c43389d65f13769d
NobuoIwase <126209407+NobuoIwase@users.noreply.github.com>
2026-09-22 11:30:10 +0900

    Bust generated asset hook cache

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
     .then(()=>load('js/generated_assets.js?v=10'))
     .then(()=>window.Game4Assets ? Game4Assets.load() : null)
     .then(()=>load('js/visual_pack.js?v=20'))
-    .then(()=>load('js/generated_asset_hooks.js?v=10'))
+    .then(()=>load('js/generated_asset_hooks.js?v=11'))
     .then(startGame4);
 })();
 function startGame4(){
