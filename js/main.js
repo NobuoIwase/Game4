d82a70f7316e5f674a9f543fb918a92b778b2fb6
NobuoIwase <126209407+NobuoIwase@users.noreply.github.com>
2026-09-22 10:07:40 +0900

    Load generated image assets before visual pack

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@
     });
   }
   load('js/quality_patch.js?v=14')
+    .then(()=>load('js/generated_assets.js?v=10'))
+    .then(()=>window.Game4Assets ? Game4Assets.load() : null)
     .then(()=>load('js/visual_pack.js?v=20'))
     .then(startGame4);
 })();
